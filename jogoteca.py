8e60be0a8d78f6c1e410ba50a113528eb6e1bc24
JonathanGamaS <jonathan_gama_@hotmail.com>
2020-06-27 22:27:17 -0300

    UPDATE: Criação de duas rotas e páginas lista e novo .html

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
+class Jogo:
+    def __init__(self, nome, categoria, plataforma):
+        self.nome = nome
+        self.categoria = categoria
+        self.plataforma = plataforma
+
 @app.route('/inicio')
 def ola():
-    return '<h1>Inicio projeto</h1>'
+    jogo1 = Jogo('World of Warcraft', 'MMO', 'PC')
+    jogo2 = Jogo('Final Fantasy XIV', 'MMO', 'PC')
+    jogo3 = Jogo('The Elder Scrolls', 'RPG', 'PC')
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo jogo')
+
 
 app.run()
 
